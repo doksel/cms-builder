619ff31f0380f046c9b4a213d97f3fb3fd2ac12e
doksel <doksel7@gmail.com>
2025-04-21 08:20:58 +0200

    connect to supabase

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -13,6 +13,7 @@ export default function LoginPage() {
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+    
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
